0890458d25bfe76bd3fa7db6ce61d9c960546e44
Ashim Bhattarai <ashimbhattarai4040@gmail.com>
2025-07-16 19:46:45 +0545

    Update AboutSection.jsx
    downloadable cv
    inserted

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
                 Get In Touch
                 </a>
                 <a
-                href="https://drive.google.com/file/d/1XYkI0LIcEPi1-3RjK4eVlVwrK5CJ7m5U/view?usp=drive_link" download=""
+                href="/projects/cv.pdf" download="cv"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                 >
                 View CV
